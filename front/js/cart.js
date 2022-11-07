81eecd18c370b405491c7ef2156372cb69d1409d
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2022-11-07 01:40:29 +0100

    affichage panier modification
    + ajout chargement de produit en fonction de leur _id.
    + ajout suppression d'un produit
    + ajout modification quantité d'un produit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,177 +1,131 @@
-// conteneur des éléméents du panier
-let itemContainer = document.getElementById("cart__items");
-let totalPriceContainer = document.getElementById("totalPrice");
-let totalQuantityContainer = document.getElementById("totalQuantity");
+/*****************
+*****  VAR   *****
+******************/
 
-// panier
+// panier localstorage
 let cart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [];
 
-// Bouton +/- quantité
-let adjustQuantityButtons = document.getElementsByClassName("itemQuantity");
-let deleteItemFromCart = document.getElementsByClassName("deleteItem");
+// conteneur HTML des éléments du panier
+let itemContainer = document.getElementById("cart__items");
 
-// Bouton soumission du panier
-let submitCart = document.getElementById("order");
+/*****************
+***  VAR END   ***
+******************/
 
+/*****************
+**  FUNCTION   ***
+******************/
 
-// affiche le panier
-function displayCart(cart, apiProductData){
-  for(let product of apiProductData){    
-    for (const order of cart) {
-      if(product._id === order._id){
-        itemContainer.innerHTML += `
-        <article class="cart__item" data-id="${product._id}" data-color="${order.color}">
-          <div class="cart__item__img">
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
+function displayCart(cart){
+
+  itemContainer.innerHTML = "";
+
+  for(item of cart){
+    itemContainer.innerHTML += 
+    `<article class="cart__item" data-id="${item._id}" data-color="${item.color}">
+      <div class="cart__item__img">
+        <img src="${item.imageUrl}" alt="${item.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${item.name}</h2>
+          <p>${item.color}</p>
+          <p>${item.price}€</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
           </div>
-          <div class="cart__item__content">
-            <div class="cart__item__content__description">
-              <h2>${product.name}</h2>
-              <p>${order.color}</p>
-              <p>${product.price}€</p>
-            </div>
-            <div class="cart__item__content__settings">
-              <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${order.quantity}">
-              </div>
-              <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-              </div>
-            </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
           </div>
-        </article>
-      `;
-    }
-  }
+        </div>
+      </div>
+    </article>`;
   }
 }
 
-function emptyHTMLElement(element){
-  element.innerHTML="";
+function updateCartLocalStorage(cart){
+  localStorage.removeItem("cart");
+  localStorage.setItem("cart",JSON.stringify(cart));
 }
 
-// affiche le prix du panier
-function displayTotalPrice(cart, apiProductData){
-  let totalPrice = 0 ;
-  for(let product of apiProductData){    
-    for (const order of cart) {
-      if(product._id === order._id){
-        totalPrice += order.quantity * product.price;
-      }
-    }
-  }
-  totalPriceContainer.innerHTML = totalPrice;
-}
+/*********************
+***  FUNCTION END  ***
+**********************/
 
-// affiche la quantité d'articles dans le paniers
-function displayTotalQuantity(cart, apiProductData){
-  let totalQuantity = 0 ;
-  for(let product of apiProductData){    
-    for (const order of cart) {
-      if(product._id === order._id){
-        totalQuantity += parseInt(order.quantity);
-      }
-    }
-  }
-  totalQuantityContainer.innerHTML = totalQuantity;
-}
+// récupération des informations  depuis l'api
+for(let i = 0; i < cart.length ; i++){
+  fetch("http://localhost:3000/api/products/" + cart[i]._id)
+  .then((response) => response.json())
+  .then(function(product){
 
-// affichage des produits 
-fetch("http://localhost:3000/api/products")
-.then((response) => response.json())
-.then(function(products){
+    cart[i].altTxt = product.altTxt;
+    cart[i].description = product.description;
+    cart[i].imageUrl = product.imageUrl;
+    cart[i].name = product.name;
+    cart[i].price = product.price;
 
-    // affichage du prix total
-    displayTotalPrice(cart, products);
-
-    // affichage de la quantité totale
-    displayTotalQuantity(cart, products);
-
-    // affichage du panier
-    displayCart(cart,products);
     
-    // modification de la quantité
-    for(let i = 0; i < adjustQuantityButtons.length; i++) {
-      adjustQuantityButtons[i].addEventListener('change', function(){
-        console.log('test');
+  })   
+  .catch(function(error){
+    alert("Une erreur est survenue lors du chargement du panier.")
+  });
+}
 
 
-        let thisProductId = this.closest("article").dataset.id;
-        let thisProductColor = this.closest("article").dataset.color;
-        
-        for (const order of cart) {
-          if(thisProductId === order._id && thisProductColor === order.color){
-            order.quantity = this.value
+setTimeout(function() {
 
-            localStorage.removeItem("cart");
-            localStorage.setItem("cart",JSON.stringify(cart));
-            
-            // // suppression des anciens elements
-            emptyHTMLElement(totalPriceContainer);
-            emptyHTMLElement(totalQuantityContainer);
+  displayCart(cart);
 
-            // // affichage des nouveaux elements
-            displayTotalPrice(cart, products);
-            displayTotalQuantity(cart, products);
-          }
+  let deleteItemFromCart = document.getElementsByClassName("deleteItem");
+  let adjustQuantityButtons = document.getElementsByClassName("itemQuantity");
+
+  // supression d'un objet du panier
+  for(let i = 0; i < deleteItemFromCart.length; i++) {
+
+    deleteItemFromCart[i].addEventListener('click', function(event){
+
+      console.log('click');
+
+      let thisProductId = this.closest("article").dataset.id;
+      let thisProductColor = this.closest("article").dataset.color;
+      
+      for (let i = 0; i < cart.length; i++) {
+        if(thisProductId === cart[i]._id && thisProductColor === cart[i].color){
+          cart.splice(i,1);
+          updateCartLocalStorage(cart);
         }
+      }
 
-        
+      alert('kanapé suprimé')
+      window.location.replace("/front/html/cart.html");
 
-      }, false);
-    }
+    }, false);
 
-    // suppression d'un objet dans une commande
-    for(let i = 0; i < deleteItemFromCart.length; i++) {
-      deleteItemFromCart[i].addEventListener('click', function(event){
-        
-        let thisProductId = this.closest("article").dataset.id;
-        let thisProductColor = this.closest("article").dataset.color;
-        
-        for (let i = 0; i < cart.length; i++) {
-          if(thisProductId === cart[i]._id && thisProductColor === cart[i].color){
+  }
 
-            cart.splice(i,1);
+  // modification de la quantité d'un objet
+  for(let i = 0; i < adjustQuantityButtons.length; i++) {
+    adjustQuantityButtons[i].addEventListener('change', function(){
 
-            localStorage.removeItem("cart");
-            localStorage.setItem("cart",JSON.stringify(cart));
-
-            // suppression des anciens elements
-            emptyHTMLElement(itemContainer);
-            emptyHTMLElement(totalPriceContainer);
-            emptyHTMLElement(totalQuantityContainer);
-
-            // affichage des nouveaux elements
-            displayCart(cart,products);
-            displayTotalPrice(cart, products);
-            displayTotalQuantity(cart, products);
-
-          }
+      let thisProductId = this.closest("article").dataset.id;
+      let thisProductColor = this.closest("article").dataset.color;
+      
+      for (let order of cart) {
+        if(thisProductId === order._id && thisProductColor === order.color){
+          order.quantity = this.value
+          updateCartLocalStorage(cart);
+          
         }
+      }
 
-        
+      
 
-        // // suppression des anciens elements
-        
-        // console.log(itemContainer);
-        // emptyHTMLElement(totalPriceContainer);
-        // emptyHTMLElement(totalQuantityContainer);
+    }, false);
+  }
+  
+}, 500);
 
-        // // affichage des nouveaux elements
-
-
-
-        
-
-
-
-        // displayTotalPrice(cart, products);
-        // displayTotalQuantity(cart, products);
-
-      }, false);
-    }
-})   
-.catch(function(error){
-    alert("Désolé, ce kanap n'est plus disponible !")
-});
+   
